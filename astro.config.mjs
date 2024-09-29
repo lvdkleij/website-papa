a13b0f62da3372e92e83c6efd96b83f77a7fd1e7
Lucas van der Kleij <kleijvander@gmail.com>
2024-09-29 14:54:19 +0200

    restructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind()],
 });
